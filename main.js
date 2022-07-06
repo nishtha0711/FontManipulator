e8c887e898ca19459decb5e22ada0cfac9029a52
nishtha0711 <105867986+nishtha0711@users.noreply.github.com>
2022-07-06 18:53:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup(){
     canvas = createCanvas(550, 450);
     canvas.position(560, 90);
@@ -9,6 +13,10 @@ function setup(){
 
 function draw(){
     background("#89CFF0");
+    textSize(difference);
+    fill('#F90093');
+    stroke('#F90093');
+    text('Nishtha', 50, 400);
 }
 
 function modelLoaded(){
@@ -18,5 +26,11 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length >0){
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
     }
+    
 }
